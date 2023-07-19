82286dc3ed35b59a41c80fcc7c1eb8c9bf4ec469
Naman1729 <namangautam000@gmail.com>
2023-07-19 18:54:54 +0530

    update

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {DecentralisedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 /**
  * @title DSCEngine
  * @author Naman Gautam
@@ -19,12 +22,77 @@ pragma solidity ^0.8.18;
  * @notice This contract is VERY loosly based on the MakerDAO DSS (DAI) system
  */
 
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
+    /* Errors */
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedAddressMustBeSameLength();
+    error DSCEngine__NotAllowedToken();
+
+    /* State Variables */
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+
+    DecentralisedStableCoin private immutable i_dsc;
+
+    /* Event */
+    event CollateralDeposited(address indexed user, address indexed token, uint256 amount);
+
+    /* Modifier */
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    /* Functions */
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        // USD Prize Feeds
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressMustBeSameLength();
+        }
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralisedStableCoin(dscAddress);
+    }
+
+    /* External Functions */
     function depositCollateralAndMintDsc() external {}
 
+    /**
+     * @notice follows CEI
+     * @param tokenCollateralAddress The address of the token to deposit as collateral
+     * @param amountCollateral The amount of collateral to deposit
+     */
+
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+    }
+
     function redeemCollateralForDsc() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDsc() external {}
 
     function burnDsc() external {}
 
     function liquidate() external {}
+
+    function getHealthFactor() external view {}
 }
